2012ff82569d5c79ba465c2a3cd1d282b12390fd
Bryan Szendel <bryan.szendel@gmail.com>
2019-12-04 18:19:09 -0700

    commands are moving the user through each room

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,10 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -25,19 +25,57 @@ earlier adventurers. The only exit is to the south."""),
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
+room['foyer'].s_to = room['outside']
+room['overlook'].s_to = room['foyer']
 room['treasure'].s_to = room['narrow']
+room['foyer'].e_to = room['narrow']
+room['narrow'].w_to = room['foyer']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player1 = Player('Bryan', room['outside'])
+
+while True:
+    print(f"{player1.name} is currently at the {player1.current_room.name}")
+    cmd = input("direction--> ")
+    if cmd == 'n':
+        if player1.current_room.name == 'Outside Cave Entrance':
+            player1.current_room = room['outside'].n_to
+        elif player1.current_room.name == 'Foyer':
+            player1.current_room = room['foyer'].n_to
+        elif player1.current_room.name == 'Narrow Passage':
+            player1.current_room = room['narrow'].n_to
+        else: 
+            print("Not allowed. Choose another move...")
+    elif cmd == 's':
+        if player1.current_room.name == 'Foyer':
+            player1.current_room = room['foyer'].s_to
+        elif player1.current_room.name == 'Grand Overlook':
+            player1.current_room = room['overlook'].s_to
+        elif player1.current_room.name == 'Treasure Chamber':
+            player1.current_room = room['treasure'].s_to
+        else:
+            print("Not allowed. Choose another move...")
+    elif cmd == 'e':
+        if player1.current_room.name == 'Foyer':
+            player1.current_room = room['foyer'].e_to
+        else:
+            print("Not allowed. Choose another move...")
+    elif cmd == 'w':
+        if player1.current_room.name == 'narrow':
+            player1.current_room = room['treasure'].w_to
+            print(player1.current_room.description)
+        else:
+            print("Not allowed. Choose another move...")
+    elif cmd == 'q':
+        print("Goodbye!")
+        break
+
 
 # Write a loop that:
 #
@@ -49,21 +87,3 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-
-while True:
-    cmd = input("direction--> ")
-    if cmd == 'n':
-        # Do something
-        print(cmd)
-    elif cmd == 's':
-        # Do something
-        print(cmd)
-    elif cmd == 'e':
-        # Do something
-        print(cmd)
-    elif cmd == 'w':
-        # Do something
-        print(cmd)
-    elif cmd == 'q':
-        print("Goodbye!")
-        break
--- src/player.py
+++ src/player.py
@@ -1,6 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
 class Player:
-    def __init__(self, name, description):
+    def __init__(self, name, current_room):
         self.name = name
-        self.description = description
+        self.current_room = current_room
+    def __str__(self):
+        return f"name: {self.name}, current_room: {self.current_room}"
